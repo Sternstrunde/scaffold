007c0cc6647da462ac97a8b267364c0616054b27
Sternstrunde <1878856344@qq.com>
2024-05-11 09:12:25 +0800

    ✨ feat: 最低 node 版本检查功能开发

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -3,14 +3,31 @@
 module.exports = core;
 
 const log = require("@test-cli-dev/log");
+const semver = require("semver");
+const colors = require("colors/safe");
 
+const contant = require("./contant");
 const pkg = require("../package.json");
 
 function core() {
   try {
     checkPkgVersion();
+    checkNodeVersion();
   } catch (e) {
     log.error(e.message);
+  }
+}
+
+function checkNodeVersion() {
+  // 获取当前版本号
+  const currentVersion = process.version;
+  // 对比最低版本号
+  const lowestVersion = contant.LOWEST_NODE_VERSION;
+
+  if (!semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(
+      colors.red(`test-cli 需要安装 v${lowestVersion} 以上版本的 Node.js`)
+    );
   }
 }
 
