a318eb5610ce3cab66258872c0ad14c6f34bfa83
Sternstrunde <1878856344@qq.com>
2024-05-11 19:56:15 +0800

    ✨ feat: 用户主目录检查功能开发

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -5,16 +5,45 @@ module.exports = core;
 const log = require("@test-cli-dev/log");
 const semver = require("semver");
 const colors = require("colors/safe");
+const userHome = require("user-home");
 
 const contant = require("./contant");
 const pkg = require("../package.json");
+const fs = require("fs");
+const fsPromises = require("fs/promises");
 
 function core() {
   try {
     checkPkgVersion();
     checkNodeVersion();
+    checkUserHome();
   } catch (e) {
     log.error(e.message);
+  }
+}
+
+function checkUserHome() {
+  if (!userHome || !pathExistsSync(userHome)) {
+    throw new Error(colors.red("当前登录用户主目录不存在!"));
+  }
+  console.log(userHome);
+}
+
+async function pathExists(path) {
+  try {
+    await fsPromises.access(path);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+function pathExistsSync(path) {
+  try {
+    fs.accessSync(path);
+    return true;
+  } catch {
+    return false;
   }
 }
 
